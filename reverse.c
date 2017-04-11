bac8c4ef420b8256c0d6ee5ad511198b706f2d2e
shobanyogi <shobanyogi1996@gmail.com>
2017-04-11 09:59:47 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main() {
+
+char a[50];
+
+int j;
+//
+printf("enter the charcater");
+
+gets(a);
+
+j=strlen(a);
+
+j-=1;
+
+while(j>=0)
+	{
+
+printf("%c",a[j]);
+
+j--;
+	}
+
+return 0;
+}
